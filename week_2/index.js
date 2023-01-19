39d41857e487ad1888246cb62e51434b048f747f
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2023-01-19 18:44:38 -0500

    person

--- week_2/index.js
+++ week_2/index.js
@@ -1,6 +1,7 @@
 const http = require("http")
 const path = require("path")
 const fs = require("fs")
+const Person = require("./person.js")
 
 const server = http.createServer((request, response) => {
     console.log(request.url)
